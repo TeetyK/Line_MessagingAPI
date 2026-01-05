9f255c9c0a0a091f5b5ecc1e8fcd953653940e78
TeetyK <teetysupergame7484@gmail.com>
2026-01-05 15:17:55 +0700

    test chatbot with LineMessagingAPI

--- main.py
+++ main.py
@@ -1,6 +1,75 @@
-def main():
-    print("Hello from line-messagingapi!")
+import os
+import google.generativeai as genai
+from flask import Flask, request, abort
+from dotenv import load_dotenv
 
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel('models/gemini-2.5-flash')
+
+app = Flask(__name__)
+
+access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+channel_secret = os.getenv("LINE_CHANNEL_SECRET")
+
+if not access_token or not channel_secret:
+    raise ValueError(".env is not found.")
+
+configuration = Configuration(access_token=access_token)
+handler = WebhookHandler(channel_secret)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature.")
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    user_message = event.message.text
+    
+    try:
+        response = model.generate_content(user_message)
+        reply_text = response.text
+    except Exception as e:
+        reply_text = f"ขออภัย เกิดข้อผิดพลาดจาก Gemini: {str(e)}"
+
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=reply_text)]
+            )
+        )
 
 if __name__ == "__main__":
-    main()
+    app.run(port=8080, debug=True)
